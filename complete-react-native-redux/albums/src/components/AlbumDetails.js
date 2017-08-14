320e8e36afca4c59e95dc06f459a830d3dbf3c63
Wei Kee <weikee070194@gmail.com>
2017-08-14 13:25:25 +0800

    add image with flexbox styling

--- complete-react-native-redux/albums/src/components/AlbumDetails.js
+++ complete-react-native-redux/albums/src/components/AlbumDetails.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headerContentStyle } = styles;
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'    
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
 };
 
 export default AlbumDetail;

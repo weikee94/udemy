fb3ad91ef112b545589257d611974ad04f209514
Wei Kee <weikee070194@gmail.com>
2017-08-14 12:01:29 +0800

    add styling to card component

--- complete-react-native-redux/albums/src/components/Card.js
+++ complete-react-native-redux/albums/src/components/Card.js
@@ -3,8 +3,26 @@ import { View } from 'react-native';
 
 const Card = () => {
     return (
-        <View></View>
+        <View style={styles.containerStyle}>
+        </View>
     );
+};
+
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
 };
 
 

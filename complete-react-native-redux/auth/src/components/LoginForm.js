d04c311a03de5d15d9961ca7d45ca2fee035fa63
Wei Kee <weikee070194@gmail.com>
2017-08-15 23:02:20 +0800

    passing props text input

--- complete-react-native-redux/auth/src/components/LoginForm.js
+++ complete-react-native-redux/auth/src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
 //state is what we do deal with user feedback
@@ -11,10 +10,9 @@ state = { text: '' };
 		return (
 			<Card>
 				<CardSection>
-					<TextInput
+					<Input
 						value={this.state.text}
 						onChangeText={text => this.setState({ text })}
-						style={{ height: 20, width: 100 }}
 					 />
 				</CardSection>
 				<CardSection />

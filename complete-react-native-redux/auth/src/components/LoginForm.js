309669b3663e4aba1df960e12af660d1d8ae2fc3
Wei Kee <weikee070194@gmail.com>
2017-08-15 22:54:47 +0800

    making text input

--- complete-react-native-redux/auth/src/components/LoginForm.js
+++ complete-react-native-redux/auth/src/components/LoginForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+//state is what we do deal with user feedback
+
+state = { text: '' };
+
 	render() {
 		return (
 			<Card>
 				<CardSection>
-					<TextInput style={{ height: 20, width: 100 }} />
+					<TextInput
+						value={this.state.text}
+						onChangeText={text => this.setState({ text })}
+						style={{ height: 20, width: 100 }}
+					 />
 				</CardSection>
 				<CardSection />
 				<CardSection>

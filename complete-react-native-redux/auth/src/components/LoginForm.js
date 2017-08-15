b3bad402c31aabb39717313a6c3b41fe1c744ea8
Wei Kee <weikee070194@gmail.com>
2017-08-15 22:30:41 +0800

    add loginForm

---
+++ complete-react-native-redux/auth/src/components/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, TextInput } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<TextInput style={{ height: 20, width: 100 }} />
+				</CardSection>
+				<CardSection />
+				<CardSection>
+					<Button>
+						Log In
+					</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
+
+export default LoginForm;

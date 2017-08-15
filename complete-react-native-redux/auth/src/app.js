c991bd113d7d581533e7841210bfa9a6121f0096
Wei Kee <weikee070194@gmail.com>
2017-08-15 22:08:32 +0800

    firebase setup copy from firebase config

--- complete-react-native-redux/auth/src/app.js
+++ complete-react-native-redux/auth/src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    //lifecycle method are method that are automatically invoked inside of our component
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyA-1MyV5d12hH1UXtr1UIqW7gPL__XmxUs',
+            authDomain: 'udemy-authentication-96113.firebaseapp.com',
+            databaseURL: 'https://udemy-authentication-96113.firebaseio.com',
+            projectId: 'udemy-authentication-96113',
+            storageBucket: 'udemy-authentication-96113.appspot.com',
+            messagingSenderId: '1031829487568'
+        });
+    }
     render() {
         return (
             <View>

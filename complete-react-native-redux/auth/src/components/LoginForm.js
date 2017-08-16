473ec82b5d4cc01672e708a2a0e708524258b34a
Wei Kee <weikee070194@gmail.com>
2017-08-16 14:19:10 +0800

    error handling loginForm

--- complete-react-native-redux/auth/src/components/LoginForm.js
+++ complete-react-native-redux/auth/src/components/LoginForm.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
 //state is what we do deal with user feedback
 
-state = { email: '', password: '' };
+state = { email: '', password: '' , error: ''};
+
+// helper method
+onButtonPress() {
+	const { email, password } = this.state;
+
+	// this will return promise(promise in javascript is a construct for handling some amount of asynchronous code, it will make request on firebase)
+	firebase.auth().signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				// in order to show error message need rerender using component level state
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+						.catch(() => {
+							this.setState({ error: 'Authentication Failed' });
+						});
+			});
+}
 
 	render() {
 		return (
@@ -26,8 +43,11 @@ state = { email: '', password: '' };
 						onChangeText={password => this.setState({ password })}
 					/>
 				</CardSection>
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log In
 					</Button>
 				</CardSection>
@@ -35,5 +55,13 @@ state = { email: '', password: '' };
 		);
 	}
 }
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
 
 export default LoginForm;

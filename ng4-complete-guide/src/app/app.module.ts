e76f97b7e4227cd98992c44def554468587dfbe6
Wei Kee <weikee070194@gmail.com>
2017-08-16 15:23:00 +0800

    create component wihtout test file ng g c recipes --spec false

--- ng4-complete-guide/src/app/app.module.ts
+++ ng4-complete-guide/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,

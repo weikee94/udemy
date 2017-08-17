d9c12a97d2c65eb6aa76aae8a41263c00366bb67
Wei Kee <weikee070194@gmail.com>
2017-08-17 15:26:05 +0800

    add recipe example

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {

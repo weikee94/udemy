45811f8473dfb0994ca51df35ea5aa6cf7b953b0
Wei Kee <weikee070194@gmail.com>
2017-08-22 21:49:19 +0800

    binding to custom events

--- ng4-complete-guide/others/cmp-databinding-start/src/app/app.component.ts
+++ ng4-complete-guide/others/cmp-databinding-start/src/app/app.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [];
 
+   onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }

e58b93d117fbc9bd2f8a3b39296b29922b7e5d89
Wei Kee <weikee070194@gmail.com>
2017-08-23 17:06:07 +0800

    add babel core and loader and dev server to live reload

--- essentials-in-javascript-es6/es6/app/index.js
+++ essentials-in-javascript-es6/es6/app/index.js
@@ -1,1 +1,1 @@
-console.log("Hello From Webpack");
+console.log("Hello From Webpacksss");
--- essentials-in-javascript-es6/es6/webpack.config.js
+++ essentials-in-javascript-es6/es6/webpack.config.js
@@ -3,5 +3,19 @@ module.exports = {
 	output: {
 		path: __dirname + './build',
 		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	},
+	devServer: {
+		port: 4000,
+		contentBase: './build',
+		inline: true
 	}
 }

78345fccf0b51148611594c84827df4dea47cebf
Wei Kee <weikee070194@gmail.com>
2017-08-23 17:27:46 +0800

    const and template literals/strings

--- essentials-in-javascript-es6/es6/app/index.js
+++ essentials-in-javascript-es6/es6/app/index.js
@@ -7,3 +7,17 @@ console.log(a);
 	let a = 'GoodBye';
 	console.log(a);
 }
+
+// const enable protection for variables that must remain the same (const cannot be reassign)
+const array = [1,2,3];
+array.push(4);
+console.log(array);
+
+// template literals/strings
+var c = 'c';
+var b = 'b';
+var d = c + '' + b;
+console.log(d);
+
+let e = `hello ${b}`;
+console.log(e);

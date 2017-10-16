1e3117861af31f6bda13ccc7ce704bb0d0977642
WeiKee <weikee070194@gmail.com>
2017-10-16 17:56:12 +0800

    private method

--- es6-in-depth-tutorial-master/app/index.js
+++ es6-in-depth-tutorial-master/app/index.js
@@ -1,29 +1,22 @@
-// Set
-let a = new Set();
-a.add(5);
-a.add(40);
-a.add("woohoo");
-a.add({x: 50, y: 200});
-console.log(a);
-console.log(a.size); // 4
-console.log(a.has(5)); // true
-console.log(a.has(7)); // false
+// private method
 
+const budget = () => {
+    let balance = 0;
+    let changeBal = (val) => {
+        return balance += val;
+    }
+    const deposit20 = () => changeBal(20);
+    const check = () => balance;
 
-let numbers = [1, 4, 5, 21, 13, 14];
-// pass array to Set constructor
-let numSet = new Set(numbers);
-console.log("set: " + numSet);
-console.log("array: " + numbers);
-
-for(let element of numSet.values()) {
-    console.log(element);
+    return {
+        deposit20: deposit20,
+        check: check
+    }
 }
 
-// checking the unique character, first split string to array then pass in the array to set constructor
-let chars = "asdaqweqwljjqwe";
-let chars_arr = chars.split("");
-let chars_set = new Set(chars_arr);
-console.log(chars_set);
-
+let wallet = budget();
+console.log(wallet);
+wallet.deposit20();
+console.log(wallet.check());
+console.log(wallet.balance);
 

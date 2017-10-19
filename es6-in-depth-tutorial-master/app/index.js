32c442e5b975a402ad62f807a4fc2e151ceac130
WeiKee <weikee070194@gmail.com>
2017-10-19 17:26:16 +0800

    es6 fetch

--- es6-in-depth-tutorial-master/app/index.js
+++ es6-in-depth-tutorial-master/app/index.js
@@ -1,25 +1,12 @@
-// Promises
+// API and ES6 Fetch
 
-let p = new Promise((resolve, reject) => {
-    // resolve and reject represent handler that give a function to update the state of the prompts resolve handles successful and fail
-    reject('Rejected');
-})
+const root = 'https://jsonplaceholder.typicode.com/posts/1';
 
-// .then method will accepts a function that gets a promise is returned value once is fulfilled
-p.then(response => console.log(response))
- .catch(error => console.log(error));
-
- let p2 = new Promise((resolve, reject) => {
-    resolve('Resolved');
- })
-
- p2.then(response => console.log(response));
+// chaining promises
+// if we return response to JSON we could handle the return JSON from this promise with another then statement
+fetch(root, { method: "GET" })
+    .then(response => response.json())
+    .then(json => console.log(json));
 
 
- let p3 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('resolve after 3 second'), 3000);
- })
- p3.then(response => console.log(response))
-   .catch(error => console.log(error));
-
-   
+    

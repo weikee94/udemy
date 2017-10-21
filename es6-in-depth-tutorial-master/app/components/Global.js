4fc30bd46dd7307eeba444884f4d3234aa0b4713
WeiKee <weikee070194@gmail.com>
2017-10-21 08:53:25 +0800

    add search method and bind to onClick function

--- es6-in-depth-tutorial-master/app/components/Global.js
+++ es6-in-depth-tutorial-master/app/components/Global.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+    search() {
+        console.log('you just clicked search');
+    }
+
     render() {
         return (
-            <div>This is From Global!</div>
+            <div>
+                <h2>Book Explorer</h2>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl type="text" placeholder="Search for a book" />
+                        <InputGroup.Addon onClick={() => this.search()}>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+            </div>
         )
     }
 }

6b27e8826c90bffb25465cd8f61f06832675c39a
WeiKee <weikee070194@gmail.com>
2017-10-21 11:39:41 +0800

    finish

--- es6-in-depth-tutorial-master/app/components/Gallery.js
+++ es6-in-depth-tutorial-master/app/components/Gallery.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
     render() {
+        let alternate = 'https://cdn4.iconfinder.com/data/icons/facebook-likes/100/8.png';
+
         return (
             <div>
                 {
                     this.props.items.map((item, index) => {
-                        let {title} = item.volumeInfo;
+                        let {title, imageLinks, infoLink} = item.volumeInfo;
                         return (
-                            <div key={index}>{title}</div>
+                            <a key={index} className="book" href={infoLink} target="_blank">
+                                <img 
+                                    src={imageLinks !== undefined ? imageLinks.thumbnail : alternate} 
+                                    alt="book image" 
+                                    className="book-img" />
+                                <div className="book-text">
+                                    {title}
+                                </div>
+                            </a>
                         )
                     })
                 }

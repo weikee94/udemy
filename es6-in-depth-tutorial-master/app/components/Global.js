02d88ad3839a13fe987df98f04fa6fd1ea43afb4
WeiKee <weikee070194@gmail.com>
2017-10-21 08:43:10 +0800

    create components

---
+++ es6-in-depth-tutorial-master/app/components/Global.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Global extends Component {
+    render() {
+        return (
+            <div>This is From Global!</div>
+        )
+    }
+}
+
+export default Global;
--- es6-in-depth-tutorial-master/app/index.js
+++ es6-in-depth-tutorial-master/app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Global from './components/Global';
 
 ReactDOM.render(
-    <div>React Application!</div>, document.getElementById('root')
+    <Global />, document.getElementById('root')
 )
     

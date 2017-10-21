8bc4d100fcda59bfbd532922f992b3e65100dcdf
WeiKee <weikee070194@gmail.com>
2017-10-21 09:12:40 +0800

    react state

--- es6-in-depth-tutorial-master/app/components/Global.js
+++ es6-in-depth-tutorial-master/app/components/Global.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
     search() {
-        console.log('you just clicked search');
+        console.log('you just clicked search', this.state.query);
     }
 
     render() {
         return (
-            <div>
+            <div className="Global">
                 <h2>Book Explorer</h2>
                 <FormGroup>
                     <InputGroup>
-                        <FormControl type="text" placeholder="Search for a book" />
+                        <FormControl type="text" placeholder="Search for a book" 
+                                    onChange={event => this.setState({query: event.target.value})}
+                                    onKeyPress={event => {
+                                        if(event.key === 'Enter') {
+                                            this.search();
+                                        }
+                                    }}/>
                         <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
@@ -24,3 +37,4 @@ class Global extends Component {
 }
 
 export default Global;
+

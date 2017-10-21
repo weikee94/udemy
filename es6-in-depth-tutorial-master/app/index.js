8dbedd0c46b1ce565537f8a952bc316e7bd99698
WeiKee <weikee070194@gmail.com>
2017-10-21 08:34:06 +0800

    init react project

--- es6-in-depth-tutorial-master/app/index.js
+++ es6-in-depth-tutorial-master/app/index.js
@@ -1,12 +1,7 @@
-// API and ES6 Fetch
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const root = 'https://jsonplaceholder.typicode.com/posts/1';
-
-// chaining promises
-// if we return response to JSON we could handle the return JSON from this promise with another then statement
-fetch(root, { method: "GET" })
-    .then(response => response.json())
-    .then(json => console.log(json));
-
-
+ReactDOM.render(
+    <div>React Application!</div>, document.getElementById('root')
+)
     

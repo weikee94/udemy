4ae53ff6ec5a48d95949b1411954b57d9369c293
WeiKee <weikee070194@gmail.com>
2017-10-24 20:44:08 +0800

    arrow functions

--- react-course-projects/indecision-app/src/playground/es6-arrow-functions2.js
+++ react-course-projects/indecision-app/src/playground/es6-arrow-functions2.js
@@ -1,8 +1,6 @@
-'use strict';
-
 // arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
     // console.log(arguments);
     return a + b;
 };
@@ -10,17 +8,13 @@ console.log(add(55, 1, 1001));
 
 // this keyword - no longer bound
 
-var user = {
+const user = {
     name: 'Andrew',
     cities: ['A', 'B', 'C'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived ' + city;
-        });
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has lived ' + city);
     }
-};
+}
 console.log(user.printPlacesLived());
 
 // Challenge area
@@ -29,16 +23,12 @@ console.log(user.printPlacesLived());
 // multiplyBy - single number
 // multiply - return a new array where the number have been multiplied
 
-var multiplier = {
+const multiplier = {
     number: [1, 5, 10],
     multiplyBy: 3,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.number.map(function (nums) {
-            return nums * _this2.multiplyBy;
-        });
+    multiply() {
+        return this.number.map((nums) => nums * this.multiplyBy);
     }
-};
+}
 
 console.log(multiplier.multiply());

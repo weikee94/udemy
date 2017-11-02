5dd3f79c81c93711d3bbb0560b6e668e447ac742
WeiKee <weikee070194@gmail.com>
2017-11-02 10:49:07 +0800

    webpack config

---
+++ react-course-projects/indecision-app/webpack.config.js
@@ -0,0 +1,14 @@
+// entry -> output
+
+const path = require('path');
+
+console.log(path.join(__dirname, 'public'));
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'      
+    }
+};
+

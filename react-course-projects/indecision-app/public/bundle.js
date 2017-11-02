b30a917d1d3ec91bca62ad6eabd942da60b1a548
WeiKee <weikee070194@gmail.com>
2017-11-02 12:13:02 +0800

    import and export

--- react-course-projects/indecision-app/public/bundle.js
+++ react-course-projects/indecision-app/public/bundle.js
@@ -65,9 +65,71 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('this is running');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person__ = __webpack_require__(2);
+// import './utils.js';
+
+// import sub, { square, add } from './utils';
+
+// console.log('this is running');
+// console.log("square: " + square(8));
+// console.log("add: " + add(12, 23));
+// console.log("subtract: " + sub(2, 1));
+
+
+// person.js
+// name export isAdult(18) - true if adult, otherwise is false
+// name export canDrink(18) - true if 18 and over, otherwise false
+
+// imoort isAdult and canDrink
+// use bvoth printing result to the console
+
+
+
+// setting the default export and function
+// grab the default and call it
+
+console.log('can drink 19: '+ __WEBPACK_IMPORTED_MODULE_0__person__["a" /* canDrink */](19));
+console.log('is adult 10: ' + __WEBPACK_IMPORTED_MODULE_0__person__["c" /* isAdult */](10));
+console.log('is senior 66: ' + __WEBPACK_IMPORTED_MODULE_0__person__["b" /* default */](64));
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isSenior; });
+const isAdult = (x) => {
+    if (x < 18) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const canDrink = (x) => {
+    if (x < 18) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const isSenior = (age) => {
+    if (age > 65) {
+        return true;
+    } else {
+        return false;
+    }
+};
+ 
+
 
 /***/ })
 /******/ ]);
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,1 +1,25 @@
-console.log('this is running');
+// import './utils.js';
+
+// import sub, { square, add } from './utils';
+
+// console.log('this is running');
+// console.log("square: " + square(8));
+// console.log("add: " + add(12, 23));
+// console.log("subtract: " + sub(2, 1));
+
+
+// person.js
+// name export isAdult(18) - true if adult, otherwise is false
+// name export canDrink(18) - true if 18 and over, otherwise false
+
+// imoort isAdult and canDrink
+// use bvoth printing result to the console
+
+import isSenior, { isAdult, canDrink } from './person';
+
+// setting the default export and function
+// grab the default and call it
+
+console.log('can drink 19: '+ canDrink(19));
+console.log('is adult 10: ' + isAdult(10));
+console.log('is senior 66: ' + isSenior(64));

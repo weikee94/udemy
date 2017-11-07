7849d12686ca0596fa6155e0ba7b7f96aa68abaf
WeiKee <weikee070194@gmail.com>
2017-11-07 17:13:49 +0800

    dynamic actions

--- react-course-projects/expensify-app/src/playground/redux-101.js
+++ react-course-projects/expensify-app/src/playground/redux-101.js
@@ -4,30 +4,36 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy =  typeof action.incrementBy === 'number' ? action.incrementBy : 1;    
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
                 count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
             }
         default:
             return state;
     }
 });
 
-console.log(store.getState());
-
-// Actions - an object that gets sent to the store
-// increment, decrement, reset
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
 // increment
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
 
 // increment
@@ -45,4 +51,14 @@ store.dispatch({
     type: 'DECREMENT'
 });
 
-console.log(store.getState());
+// decrement
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
+
+// set
+store.dispatch({
+    type: 'SET',
+    count: 101
+});

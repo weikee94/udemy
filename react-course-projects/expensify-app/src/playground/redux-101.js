d9191084ad9fb114ef1aa17167940b52ba5b083e
WeiKee <weikee070194@gmail.com>
2017-11-07 11:53:11 +0800

    dispatching actions

--- react-course-projects/expensify-app/src/playground/redux-101.js
+++ react-course-projects/expensify-app/src/playground/redux-101.js
@@ -1,13 +1,48 @@
 import { createStore } from 'redux';
 
 // redux state container
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state;
+    }
 });
 
 console.log(store.getState());
 
-// old way to set state
-// this.setState((prevState) => {
-//     return prevState;
-// });
+// Actions - an object that gets sent to the store
+// increment, decrement, reset
+
+// increment
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+// increment
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+// reset
+store.dispatch({
+    type: 'RESET'
+});
+
+// decrement
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+console.log(store.getState());

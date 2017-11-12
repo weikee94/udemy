20079c16c397b08fa7ee746fa49da5e7064e2e6b
WeiKee <weikee070194@gmail.com>
2017-11-12 11:19:28 +0800

    remove item using connect default and without pass in mapStateToProps

--- react-course-projects/expensify-app/src/components/ExpenseListItem.js
+++ react-course-projects/expensify-app/src/components/ExpenseListItem.js
@@ -1,11 +1,16 @@
 // export a stateless functional component
 // description, amount, createdAt
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
  
-const ExpenseListItem = ( { description, amount, createdAt } ) => (
+const ExpenseListItem = ( { dispatch, id, description, amount, createdAt } ) => (
     <div>
         <h3>{ description }</h3>
         <p>{ amount } - { createdAt }</p>
+        <button onClick={() => {
+            dispatch(removeExpense({ id: id }));
+        }}>Remove</button>
     </div>
-);
-export default ExpenseListItem;
+)
+export default connect()(ExpenseListItem);

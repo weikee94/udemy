696a22ec289f397e4815cb3edd0b6e98b4dc1f2d
WeiKee <weikee070194@gmail.com>
2017-11-14 07:13:14 +0800

    wiring add expense

--- react-course-projects/expensify-app/src/routers/AppRouter.js
+++ react-course-projects/expensify-app/src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import { ExpenseDashboardPage } from '../components/ExpenseDashboardPage';
-import { AddExpensePage } from '../components/AddExpensePage';
+import AddExpensePage from '../components/AddExpensePage';
 import { EditExpensePage } from '../components/EditExpensePage';
 import { HelpPage } from '../components/HelpPage';
 import { NotFoundPage } from '../components/NotFoundPage';

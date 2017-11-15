8e91249e5b7be6c2faf9a34687e09b9f870479b0
WeiKee <weikee070194@gmail.com>
2017-11-15 23:11:22 +0800

    filter dates

--- react-course-projects/expensify-app/src/components/ExpenseListFilters.js
+++ react-course-projects/expensify-app/src/components/ExpenseListFilters.js
@@ -1,30 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters';
 
-const ExpenseListFilters = (props) => (
-    <div>
-        <input type="text" value={props.filters.text}
-            onChange={(e) => {
-                props.dispatch(setTextFilter(e.target.value));
-                console.log(e.target.value)
-            }}
-        />
-        <select value={props.filters.sortBy}
-                onChange={(e) => {
-                    if (e.target.value === 'date') {
-                        props.dispatch(sortByDate());
-                    } else if (e.target.value === 'amount') {
-                        props.dispatch(sortByAmount());
-                    }
-                }}
-        >
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-        </select>
-        
-    </div>
-);
+class ExpenseListFilters extends React.Component {
+    state = {
+        calendarFocused: null
+    };
+    onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    };
+    onFocusChange = (calendarFocused) => {
+        this.setState(() => ({
+            calendarFocused: calendarFocused
+        }));
+    };
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.props.filters.text}
+                    onChange={(e) => {
+                        this.props.dispatch(setTextFilter(e.target.value));
+                        console.log(e.target.value)
+                    }}
+                />
+                <select value={this.props.filters.sortBy}
+                        onChange={(e) => {
+                            if (e.target.value === 'date') {
+                                this.props.dispatch(sortByDate());
+                            } else if (e.target.value === 'amount') {
+                                this.props.dispatch(sortByAmount());
+                            }
+                        }}
+                >
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
+                </select>
+                <DateRangePicker 
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                    showClearDates={true}
+                />
+            </div>
+        );
+    }
+}
 
 // setup value and onChange for select 
 
--- react-course-projects/expensify-app/src/selectors/expenses.js
+++ react-course-projects/expensify-app/src/selectors/expenses.js
@@ -1,26 +1,10 @@
-// get visible expenses
-// const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
-//     return expenses.filter((expense) => {
-//         const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-//         const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
-//         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-
-//         return startDateMatch && endDateMatch && textMatch;
-//         }).sort((a, b) => {
-//             if (sortBy === 'date') {
-//                 return a.createdAt < b.createdAt ? 1 : -1;
-//             } else if(sortBy === 'amount') {
-//                 return a.amount < b.amount ? 1 : -1;
-//             }
-//     });
-// };
-
-// export { getVisibleExpenses };
+import moment from 'moment';
 
 export default (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
-      const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-      const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+      const createdAtMoment = moment(expense.createdAt);
+      const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+      const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true ;
       const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
   
       return startDateMatch && endDateMatch && textMatch;
